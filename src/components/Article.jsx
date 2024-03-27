8e1526e528bcd78061e1e9b9aebb9fc74ac520dc
Guga Karalashvili <guga.karalashvili@gmail.com>
2024-03-27 23:06:46 +0400

    SES-7 add 'Profile' page, enhace 'Blog page'

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,11 +1,26 @@
-const Article = ({ title, text }) => {
+const Article = ({ title, text, image, createDate }) => {
     return (
         <div className="mb-10 border-b border-light_blue">
-            <h3 className="text-[30px] text-left break-words break-normal mb-[20px]">{title}</h3>
+            <h3 className="w-fit border-b-[3px] border-light_blue text-[30px] text-start break-words break-normal mb-[20px]">{title}</h3>
 
-            <div className="py-[15px]">
-                {text}
+            <div className="w-full flex gap-5 items-center">
+                <div className="w-1/4">
+                    <img src={image} alt="blog" className="w-full h-full object-cover" />
+                </div>
+
+                <div className="py-[15px] w-3/4 flex flex-col gap-2">
+                    <div className="flex gap-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+
+                        <span className="text-light_blue">{createDate}</span>
+                    </div>
+                    {text}
+                    <a href="#" className="w-fit text-[20px] text-light_blue hover:text-orange transition-all duration-300 ease-in-out">Read more ...</a>
+                </div>
             </div>
+
         </div>
     );
 }
---
+++ src/components/pages/profile/Profile.jsx
@@ -0,0 +1,101 @@
+const Profile = () => {
+
+    return (
+        <>
+            <h2 className="text-[40px] leading-[25px] text-center my-[20px]">Edit Your Profile Data</h2>
+            <form className="w-1/5 border-2 border-light_blue shadow-xl shadow-light_blue p-8 rounded-xl flex flex-col gap-3 justify-between items-start mx-auto">
+
+                <input
+                    className="w-full h-full border-2 border-light_blue py-3 pl-1 text-secondary lg:text-[16px] rounded-[5px] focus:border-orange focus:outline-orange"
+                    maxLength="100"
+                    name="firstname"
+                    placeholder="Firstname"
+                    type="text"
+                    autoCapitalize="none"
+                    autoComplete="off"
+                    spellCheck="false"
+                />
+
+                <input
+                    className="w-full h-full border-2 border-light_blue  py-3 pl-1 text-secondary lg:text-[16px] rounded-[5px] focus:border-orange focus:outline-orange"
+                    maxLength="100"
+                    name="lastname"
+                    placeholder="Lastname"
+                    type="text"
+                    autoCapitalize="none"
+                    autoComplete="off"
+                    spellCheck="false"
+                />
+
+                <input
+                    className="w-full h-full border-2 border-light_blue py-3 pl-1 text-secondary lg:text-[16px] rounded-[5px] focus:border-orange focus:outline-orange"
+                    maxLength="100"
+                    name="email"
+                    placeholder="Email"
+                    type="text"
+                    autoCapitalize="none"
+                    autoComplete="off"
+                    spellCheck="false"
+                />
+
+                <input
+                    className="w-full h-full border-2 border-light_blue py-3 pl-1 text-secondary lg:text-[16px] rounded-[5px] focus:border-orange focus:outline-orange"
+                    maxLength="100"
+                    name="mobile"
+                    placeholder="Mobile"
+                    type="text"
+                    autoCapitalize="none"
+                    autoComplete="off"
+                    spellCheck="false"
+                />
+
+                <input
+                    className="w-full h-full border-2 border-light_blue py-3 pl-1 text-secondary lg:text-[16px] rounded-[5px] focus:border-orange focus:outline-orange"
+                    maxLength="100"
+                    name="currentPassword"
+                    placeholder="Current Password"
+                    type="password"
+                    autoCapitalize="none"
+                    autoComplete="off"
+                    spellCheck="false"
+                    required
+                />
+
+                <input
+                    className="w-full h-full border-2 border-light_blue py-3 pl-1 text-secondary lg:text-[16px] rounded-[5px] focus:border-orange focus:outline-orange"
+                    maxLength="100"
+                    name="newPassword"
+                    placeholder="New Password"
+                    type="password"
+                    autoCapitalize="none"
+                    autoComplete="off"
+                    spellCheck="false"
+                    required
+                />
+
+                <input
+                    className="w-full h-full border-2 border-light_blue py-3 pl-1 text-secondary lg:text-[16px] rounded-[5px] focus:border-orange focus:outline-orange"
+                    maxLength="100"
+                    name="currentNewPassword"
+                    placeholder="Confirm New Password"
+                    type="password"
+                    autoCapitalize="none"
+                    autoComplete="off"
+                    spellCheck="false"
+                    required
+                />
+
+                <div className="w-full flex gap-3">
+                    <button className="w-full h-full border-0 bg-red-800 py-3 mx-auto text-white lg:text-[20px] rounded-[5px] hover:bg-orange transition-all transform duration-300 ease-linear">
+                        Cancel
+                    </button>
+                    <button className="w-full h-full border-0  bg-blue-500 py-3 mx-auto text-white lg:text-[20px] rounded-[5px] hover:bg-orange transition-all transform duration-300 ease-linear">
+                        Save
+                    </button>
+                </div>
+            </form>
+        </>
+    );
+}
+
+export default Profile;
