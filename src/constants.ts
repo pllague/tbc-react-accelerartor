4f56e5d6f08b5901dd7b0e3e382c7817c90abdf9
Guga Karalashvili <guga.karalashvili@gmail.com>
2024-05-09 13:34:30 +0400

    SES-17 update remote url

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const AUTH_COOKIE_KEY = "auth";
-export const BASE_URL = process.env.VERCEL_ENV === "development" ? "http://localhost:3000" : "https://tbc-react-accelerartor-pllagues-projects.vercel.app/";
+export const BASE_URL = process.env.VERCEL_ENV === "development" ? "http://localhost:3000" : "https://tbc-react-accelerartor-pllagues-projects.vercel.app";
