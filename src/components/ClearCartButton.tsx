cde8908657d0886bc098b82e10f1f890df5ceee8
Guga Karalashvili <guga.karalashvili@gmail.com>
2024-05-19 14:29:53 +0400

    SES-19 style fixes, display 'TOTAL' price on 'Cart' page

--- src/components/ClearCartButton.tsx
+++ src/components/ClearCartButton.tsx
@@ -1,18 +1,19 @@
 "use client";
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import { clearCartAction } from "../app/actions";
 
 const ClearCartButton = () => {
   const locale = useLocale();
+  const t = useTranslations("Index");
   return (
     <button
       onClick={() => clearCartAction()}
       className={
-        " bg-yellow-600 dark:bg-blue-500 hover:bg-orange rounded-[100px] py-2 lg:py-auto px-7 font-small lg:font-medium cursor-pointer transition-all transform duration-300 ease-linear" +
+        " bg-yellow-600 dark:bg-blue-500 hover:bg-orange dark:hover:bg-orange rounded-[100px] py-2 lg:py-auto px-7 font-small lg:font-medium cursor-pointer transition-all transform duration-300 ease-linear" +
         (locale === "ka" ? " lg:text-[16px]" : " lg:text-[18px]")
       }
     >
-      {locale === "en" ? "Clear" : "გასუფთავება"}
+      {t("clear")}
     </button>
   );
 };
--- src/components/LogOut.tsx
+++ src/components/LogOut.tsx
@@ -1,12 +1,24 @@
-'use client';
+"use client";
 const LogOut = () => {
-    return (
-        <form action="/api" method="GET">
-            <button className="group mt-[5px]"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 group-hover:stroke-orange transition-all transform duration-300 ease-linear">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
-            </svg>
-            </button>
-        </form>
-    )
-}
+  return (
+    <form action="/api" method="GET">
+      <button className="group mt-[5px]">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-7 h-7 group-hover:stroke-orange transition-all transform duration-300 ease-linear"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+          />
+        </svg>
+      </button>
+    </form>
+  );
+};
 export default LogOut;
