4d0fa6cb198911a48b65db8e48c35e68b551d815
Guga Karalashvili <guga.karalashvili@gmail.com>
2024-06-05 00:42:17 +0400

    SES-20 fix issues while getting data from db

--- src/app/[locale]/(dashboard)/profile/page.tsx
+++ src/app/[locale]/(dashboard)/profile/page.tsx
@@ -9,8 +9,6 @@ export const metadata = {
 const ProfilePage = async () => {
   const userImage = await getUserImage();
   const userInfo = await getUserInfo();
-  console.log("user image ------------------ :" + userImage);
-  console.log("user data ------------------ :" + userInfo);
   return (
     <div className="px-[4%] pb-24 flex justify-center items-center md:flex-col gap-20 md:gap-12">
       <div className="flex flex-col gap-3 justify-center items-center">
@@ -19,14 +17,14 @@ const ProfilePage = async () => {
       <div className="flex flex-col justify-center gap-8">
         <div className="flex border border-blue-500 p-4 xs:p-2 rounded-lg gap-8 xs:gap-3 shadow-lg bg-white items-baseline dark:bg-slate-900">
           <div className="flex flex-1 justify-center items-center"></div>
-          {/* <div className="font-bold text-blue-500">
+          <div className="font-bold text-blue-500">
             {userInfo?.name && (
               <h3 className="text-xl  leading-6 h-8">Full Name</h3>
             )}
             {userInfo?.email && (
               <h3 className="text-xl leading-6 h-8">Email</h3>
             )}
-          </div> */}
+          </div>
           <ProfileInfo user={userInfo} />
         </div>
       </div>
