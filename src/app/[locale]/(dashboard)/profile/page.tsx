de8b78a2d5352e571495df17cae5d4ef8911a8eb
Guga Karalashvili <guga.karalashvili@gmail.com>
2024-06-05 00:00:08 +0400

    SES-20 refactoring code

--- src/app/[locale]/(dashboard)/profile/page.tsx
+++ src/app/[locale]/(dashboard)/profile/page.tsx
@@ -1,6 +1,6 @@
 import AvatarUpload from "../../../../components/AvatarUpload";
-import { getUserImage } from "../../../api";
-// import ProfileInfo from "../../../../components/ProfileInfo";
+import { getUserImage, getUserInfo } from "../../../api";
+import ProfileInfo from "../../../../components/ProfileInfo";
 export const metadata = {
   title: "Profile",
   description: "Profile by Next",
@@ -8,7 +8,7 @@ export const metadata = {
 
 const ProfilePage = async () => {
   const userImage = await getUserImage();
-  // const userInfo = await getUserInfo();
+  const userInfo = await getUserInfo();
 
   return (
     <div className="px-[4%] pb-24 flex justify-center items-center md:flex-col gap-20 md:gap-12">
@@ -18,13 +18,15 @@ const ProfilePage = async () => {
       <div className="flex flex-col justify-center gap-8">
         <div className="flex border border-blue-500 p-4 xs:p-2 rounded-lg gap-8 xs:gap-3 shadow-lg bg-white items-baseline dark:bg-slate-900">
           <div className="flex flex-1 justify-center items-center"></div>
-          {/* <div className="font-bold text-blue-500">
-            {userInfo.name && (
+          <div className="font-bold text-blue-500">
+            {userInfo?.name && (
               <h3 className="text-xl  leading-6 h-8">Full Name</h3>
             )}
-            {userInfo.email && <h3 className="text-xl leading-6 h-8">Email</h3>}
-          </div> */}
-          {/* <ProfileInfo user={userInfo} /> */}
+            {userInfo?.email && (
+              <h3 className="text-xl leading-6 h-8">Email</h3>
+            )}
+          </div>
+          <ProfileInfo user={userInfo} />
         </div>
       </div>
     </div>
