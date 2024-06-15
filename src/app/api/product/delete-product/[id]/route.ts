ea03d3c4113c63d5b30bbcdbc9a2146fd78ade8a
Guga Karalashvili <guga.karalashvili@gmail.com>
2024-06-15 22:03:07 +0400

    FP-5 add functionality to remove product from cart if product is deleted from the site

--- src/app/api/product/delete-product/[id]/route.ts
+++ src/app/api/product/delete-product/[id]/route.ts
@@ -1,16 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
-import { sql } from '@vercel/postgres';
+import { sql } from "@vercel/postgres";
+import { deleteProductFromCartAction } from "../../../../actions";
 
+export async function DELETE(
+  _: NextRequest,
+  { params: { id } }: { params: { id: number } }
+) {
+  try {
+    if (!id) throw new Error("ID is required");
 
-export async function DELETE(_: NextRequest, { params: { id } }: { params: { id: number }}) {
-    try {
-        if(!id) throw new Error('ID is required');
+    await sql`DELETE FROM products WHERE id = ${Number(id)};`;
+    // Delete related reviews and ratings
+    await sql`DELETE FROM reviews WHERE product_id = ${id};`;
+    await sql`DELETE FROM ratings WHERE product_id = ${id};`;
 
-        await sql`DELETE FROM products WHERE id = ${Number(id)};`;
-    } catch (error) {
-        return NextResponse.json({error}, {status: 500});
-    }
+    // Remove product from the carts table
+    await deleteProductFromCartAction(id);
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
 
-    const products = await sql`SELECT * FROM products;`;
-    return NextResponse.json({products}, {status: 200});
+  const products = await sql`SELECT * FROM products;`;
+  return NextResponse.json({ products }, { status: 200 });
 }
