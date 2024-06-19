f360dced9ea6d0e896433fbc00756f2b5ffb7144
Guga Karalashvili <guga.karalashvili@gmail.com>
2024-06-19 12:12:12 +0400

    FP-7 clear cache on getUserInfo route

--- src/app/api/create-user/route.ts
+++ src/app/api/create-user/route.ts
@@ -3,16 +3,18 @@ import { NextRequest } from "next/server";
 import { getSession } from "@auth0/nextjs-auth0";
 import { redirect } from "next/navigation";
 
+export const revalidate = 0;
+
 export async function GET(_: NextRequest) {
   try {
     const session = await getSession();
 
     if (session?.user) {
-      const { sub, name ,email, picture } = session.user;
+      const { sub, name, email, picture } = session.user;
 
       const user = await sql`SELECT * FROM users WHERE sub = ${sub}`;
 
-      if (!user.rows.length){
+      if (!user.rows.length) {
         await sql`INSERT INTO users (name, email, sub, image_url) VALUES ( ${name}, ${email}, ${sub}, ${picture});`;
       }
     }
